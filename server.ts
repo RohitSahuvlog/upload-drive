db814b206790b553f2c002697d172565f97f13d6
Rohit Sahu <rohit.s@juegostudioz.com>
2023-03-21 16:00:20 +0530

    update

--- server.ts
+++ server.ts
@@ -3,27 +3,19 @@ import methodOverride from "method-override";
 import bodyParser from "body-parser";
 import router from "./Router/imageRouter";
 import path from "path";
-import userRoutes from  "./Router/userRoutes"
+import userRoutes from "./Router/userRoutes";
 import authentication from "./Middleware/authentication";
-import uploadauthentication from "./Middleware/uploadmiddlewere";
 import uploadauth2 from "./Middleware/uploadmiddle2";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 
-
 app.use(methodOverride("X-HTTP-Method-Override"));
 // Public
-app.use("/uploads", express.static(path.resolve("uploads")));
 app.use("/api/user", userRoutes);
 app.use(authentication);
 app.use("/uploadimage", router);
-app.use(
-  "/uploads",
-  uploadauthentication,
-  express.static(path.resolve("uploads"))
-);
 app.use("/uploads", uploadauth2, express.static(path.resolve("uploads")));
 const port = 3000;
 console.log("check");
