a8bfd1706b504613951a68f10f0cb23a5541cfad
Rohit Sahu <rohit.s@juegostudioz.com>
2023-03-27 14:54:52 +0530

    remove file type

--- Router/fileRouter.ts
+++ Router/fileRouter.ts
@@ -13,16 +13,9 @@ import uploadauthentication from "../Middleware/uploadmiddlewere";
 
 const router = Router();
 const fileFilter = (req: any, file: any, cb: any) => {
-  if (
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/jpeg" ||
-    file.mimetype === "image/png" ||
-    "image/pdf"
-  ) {
+ 
     cb(null, true);
-  } else {
-    cb(new Error("Image uploaded is not of type jpg/jpeg or png"), false);
-  }
+ 
 };
 const upload = multer({ storage: storage, fileFilter: fileFilter });
 router.get("/get", getFile);
