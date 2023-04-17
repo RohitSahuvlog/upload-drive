4eb04133dc3c6bba7601ed132900576547230106
Rohit Sahu <rohit.s@juegostudioz.com>
2023-04-17 12:26:31 +0530

    remove addactivity

--- Controller/permissionController.ts
+++ Controller/permissionController.ts
@@ -1,6 +1,5 @@
 import { Response, Request } from "express";
 import { UploadRequest } from "../Config/uploadRequest";
-import { File } from "../db_helper/file";
 import { Permission } from "../db_helper/permission";
 import { User } from "../db_helper/user";
 
@@ -8,10 +7,7 @@ export const addPermisions = async (req: Request, res: Response) => {
   const uploadReq = req as UploadRequest;
   const { permissionType, email } = req.body;
   const filepath = uploadReq.params.id;
-  let ip: any =
-    (uploadReq.headers["x-forwarded-for"] as string)?.split(",")[1] ||
-    uploadReq.connection.remoteAddress;
-  let useragent = uploadReq.headers["user-agent"] as string;
+
   try {
     if (!filepath || !permissionType || !email) {
       return res.status(400).send({ message: "Please Enter all the Feilds" });
@@ -32,24 +28,9 @@ export const addPermisions = async (req: Request, res: Response) => {
       permissionType
     );
 
-    let activity = await Permission.addActivity(
-      "ADD_PERMISSION",
-      userid,
-      ip.split(":")[3],
-      useragent,
-      filepath,
-      `User ${userid} was granted permission ${permissionType} to access file ${filepath} by user ${uploadReq.userId}`
-    );
     return res.status(201).send({ message: "you have an access of this file" });
   } catch (error) {
-    await Permission.addActivity(
-      "ERROR",
-      0,
-      ip.split(":")[3],
-      useragent,
-      filepath,
-      `Error occurred while logging activity: ${error}`
-    );
+   
     return res.status(500).send({ error });
   }
 };
@@ -91,24 +72,10 @@ export const updatePermission = async (req: Request, res: Response) => {
     if (addpermission[1] === 0) {
       return res.status(404).send({ message: "User have not authorized" });
     }
-    let activity = await Permission.addActivity(
-      "UPDATE_PERMISSION",
-      userid,
-      ip.split(":")[3],
-      useragent,
-      filepath,
-      `User ${userid} was granted  update permission ${permissionType} to access file ${filepath} by user ${uploadReq.userId}`
-    );
+   
     return res.send({ message: "permission has been updated successfully" });
   } catch (error) {
-    await Permission.addActivity(
-      "ERROR",
-      0,
-      ip.split(":")[3],
-      useragent,
-      filepath,
-      `Error occurred while logging activity: ${error}`
-    );
+   
     return res.status(500).send({ error });
   }
 };
@@ -135,25 +102,10 @@ export const removePermissions = async (req: Request, res: Response) => {
       userid,
       filepath
     );
-    let activity = await Permission.addActivity(
-      "DELETE_PERMISSION",
-      userid,
-      ip.split(":")[3],
-      useragent,
-      filepath,
-      `User ${userid} was granted  remove permission  to the file ${filepath} by user ${uploadReq.userId}`
-    );
 
     return res.send({ message: "Permission removed from given user" });
   } catch (error) {
-    await Permission.addActivity(
-      "ERROR",
-      0,
-      ip.split(":")[3],
-      useragent,
-      filepath,
-      `Error occurred while logging activity: ${error}`
-    );
+   
     res.status(500).send({ message: "file donot present in database" });
   }
 };
--- db_helper/permission.ts
+++ db_helper/permission.ts
@@ -79,16 +79,5 @@ export class Permission {
     return result;
   }
 
-  static async addActivity(
-    action: String,
-    user_id: number,
-    ipaddress: String,
-    useragent: String,
-    filename:String,
-    status:String
-  ) {
-    let sql = `INSERT INTO Activity   (action,user, ipaddress,user_agent,filename,status) VALUES ("${action}",${user_id}, "${ipaddress}","${useragent}","${filename}","${status}") `;
-    var result = await sequelize.query(sql, { type: QueryTypes.INSERT });
-    return result;
-  }
+
 }
